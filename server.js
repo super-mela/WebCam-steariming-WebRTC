2cc8e4d9c15e44cd17a922864483132dbdcee2e4
melaku <genuieman@gmail.com>
2022-08-13 18:04:52 +0300

    http video stream

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 const bodyParser = require('body-parser');
 const webrtc = require("wrtc");
-const cors = require('cors')
-const path = require('path')
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 let senderStream;
 
@@ -62,9 +70,49 @@ app.post('/broadcast', async ({ body }, res) => {
     res.json(payload);
 });
 
+app.get('/video', (res, req) => {
+
+    const range = req.req.headers.range;
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1 * 1e+6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start * chunkSize, videoSize - 1);
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Range": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4"
+    }
+
+    res.res.writeHead(206, headers);
+
+    const stream = fs.createReadStream(videoPath, { start, end })
+    stream.pipe(res.res);
+})
+
 function handleTrackEvent(e, peer) {
     senderStream = e.streams[0];
 };
+let message = ''
+io.on("connection", (socket) => {
+
+    console.log("A user Connected")
+    console.log('message: ' + socket.id);
+    socket.on('liveEvent', (msg) => {
+
+        message = msg;
+        io.emit("liveEvent", { message: msg })
+    });
+    io.emit("liveEvent", { message: message })
+
+    socket.on("disconnect", (data) => {
+        console.log("A user disconnected" + (socket.id))
+    });
+});
 
 
-app.listen(4001, () => console.log('server started'));
+
+server.listen(4001, () => console.log('server started'));
